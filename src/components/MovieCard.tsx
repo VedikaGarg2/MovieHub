05e086ba9a7800224a9216574a039310d5135b77
VedikaGarg2 <vedika2.garg@gmail.com>
2025-03-03 12:04:23 -0700

    Displaying Critic Score

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { Movie } from "../hooks/useMovies";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+import CriticScore from "./CriticScore";
 
 interface Props {
   movie: Movie;
 }
 const MovieCard = ({ movie }: Props) => {
   const imageURL = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+  const percentage = parseInt((movie.vote_average * 10).toFixed(0), 10);
+
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={imageURL} alt={movie.title} />
       <CardBody>
         <Heading fontSize="2xl">{movie.title}</Heading>
+        <HStack justifyContent="right">
+          <CriticScore score={percentage} />
+        </HStack>
       </CardBody>
     </Card>
   );
--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -7,6 +7,7 @@ export interface Movie {
     title: string;
     poster_path: string;
     backdrop_path: string;
+    vote_average: number;
   }
   
   interface FetchMoviesResponse {
