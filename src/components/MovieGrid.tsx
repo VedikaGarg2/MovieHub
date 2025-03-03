5ead4789f5f4ab12828e4995760445a0288ebdac
VedikaGarg2 <vedika2.garg@gmail.com>
2025-03-03 12:46:35 -0700

    Show Loading Skeletons

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,9 +1,11 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 import useMovies from "../hooks/useMovies";
 import MovieCard from "./MovieCard";
+import MovieCardSkeleton from "./MovieCardSkeleton";
 
 const MovieGrid = () => {
-  const { movies, error } = useMovies();
+  const { movies, error, isLoading } = useMovies();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
@@ -13,6 +15,8 @@ const MovieGrid = () => {
         padding={10}
         spacing={10}
       >
+        {isLoading &&
+          skeletons.map((skeleton) => <MovieCardSkeleton key={skeleton} />)}
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -17,19 +17,26 @@ export interface Movie {
 const useMovies = () => {
     const [movies, setMovies] = useState<Movie[]>([]);
     const [error, setError] = useState("");
-    
+    const [isLoading, setLoading] = useState(false);
+
     useEffect(() => {
         const controller = new AbortController();
 
+        setLoading(true);
         apiClient
          .get<FetchMoviesResponse>("/movie/popular", { signal: controller.signal })
-         .then((res) => setMovies(res.data.results))
+         .then((res) => {
+            setMovies(res.data.results);
+            setLoading(false);
+        })
          .catch((err) => {
             if (err instanceof CanceledError) return;
-            setError(err.message)});
+            setError(err.message);
+            setLoading(false);
+        });
 
         return () => controller.abort();
     }, []);
-    return {movies, error};
+    return {movies, error, isLoading};
 }
 export default useMovies;
